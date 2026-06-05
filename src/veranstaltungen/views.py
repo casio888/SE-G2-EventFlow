0a8e0d86d0c852d136bb26ef06297a2e9effe986
Nina <nsteyer95@gmail.com>
2026-06-05 23:41:18 +0200

    feature: Views auf echte Datenbankabfragen umgestellt

--- src/veranstaltungen/views.py
+++ src/veranstaltungen/views.py
@@ -1,24 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from veranstaltungen.models import Veranstaltung
 
 def veranstaltungen_liste(request):
-    dummy_events = [
-        {"id": 1, "titel": "Event 1", "ort": "Dresden"},
-        {"id": 2, "titel": "Event 2", "ort": "Berlin"},
-    ]
-    return render(request, "veranstaltungen/liste.html", {"events": dummy_events})
+    events = Veranstaltung.objects.all()
+    return render(request, "veranstaltungen/liste.html", {"events": events})
 
 def veranstaltung_erstellen(request):
     return render(request, "veranstaltungen/erstellen.html")
 
 def veranstaltung_detail(request, id):
-    dummy_event = {
-        "id": id,
-        "titel": f"Event {id}",
-        "ort": "Dresden",
-        "start_datum": "2026-06-01",
-        "end_datum": "2026-06-02",
-        "beschreibung": "Beschreibung folgt",
-        "bewerber": [],
-    }
-    return render(request, "veranstaltungen/detail.html", {"event": dummy_event})
+    event = get_object_or_404(Veranstaltung, pk=id)
+    return render(request, "veranstaltungen/detail.html", {"event": event})
