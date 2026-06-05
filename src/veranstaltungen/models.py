e5cd99bf0e240d2fccbd3144838e1e19b853fc9d
Nina <nsteyer95@gmail.com>
2026-06-05 23:12:46 +0200

    feature: Basisstruktur für Veranstaltungen

--- src/veranstaltungen/models.py
+++ src/veranstaltungen/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Veranstaltung(models.Model):
+    titel = models.CharField(max_length=200)
+    beschreibung = models.TextField(blank=True)
+    start_datum = models.DateField()
+    end_datum = models.DateField()
+    ort = models.CharField(max_length=200)
+    erstellt_am = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titel
--- src/veranstaltungen/views.py
+++ src/veranstaltungen/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def veranstaltungen_liste(request):
+    dummy_events = [
+        {"id": 1, "titel": "Event 1", "ort": "Dresden"},
+        {"id": 2, "titel": "Event 2", "ort": "Berlin"},
+    ]
+    return render(request, "veranstaltungen/liste.html", {"events": dummy_events})
+
+def veranstaltung_erstellen(request):
+    return render(request, "veranstaltungen/erstellen.html")
+
+def veranstaltung_detail(request, id):
+    dummy_event = {
+        "id": id,
+        "titel": f"Event {id}",
+        "ort": "Dresden",
+        "start_datum": "2026-06-01",
+        "end_datum": "2026-06-02",
+        "beschreibung": "Beschreibung folgt",
+        "bewerber": [],
+    }
+    return render(request, "veranstaltungen/detail.html", {"event": dummy_event})
