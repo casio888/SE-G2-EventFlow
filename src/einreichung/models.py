021d3aedcd40da84e7658da81960826ace6cf435
Elias lange <s88205@htw-dresden.de>
2026-06-22 11:29:29 +0200

    test: Test für die einreichung (Startseite) erstellt und getestet.

--- src/einreichung/models.py
+++ src/einreichung/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils import timezone
 from veranstaltungen.models import Veranstaltung
 
-class Einreichung():
+class Einreichung(models.Model):
     fk=models.ForeignKey(Veranstaltung, on_delete=models.CASCADE) 
-    eingereicht=models.BooleanField()
-    Beschreibung=models.CharField(max_length="400")
-    Anforderungen=models.TextField(max_length="400")
+    eingereicht=models.BooleanField(default=False)
+    Beschreibung=models.CharField(max_length=400)
+    Anforderungen=models.TextField(max_length=400)
     def __str__(self):
         return self.id
+    def ist_in_zukunft(self):
+        """Gibt True zurück, wenn die Veranstaltung in der Zukunft liegt"""
+        return self.fk.start_datum >= timezone.now().date()
+    def ist_in_vergangenheit(self):
+        """Gibt True zurück, wenn die VEranstaltung in der Vergangenheiit liegt"""
+        return self.fk.end_datum < timezone.now().date()
 # Create your models here.
--- src/einreichung/tests.py
+++ src/einreichung/tests.py
@@ -1,3 +1,37 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+#from django.urls import reverse
+from .models import Einreichung
+from veranstaltungen.models import Veranstaltung
+
+class QuestionModelTests(TestCase):
+    # Soll nicht anzeigen, was in der Vergangenheit liegt
+    def test_event_in_past(self):
+        """Gibt True zurück, wenn die Veranstaltung vorbei ist"""
+        past_date= (timezone.now()- datetime.timedelta(days=1)).date()
+
+        veranstaltung=Veranstaltung.objects.create(
+            titel="Test event",
+            start_datum=past_date-datetime.timedelta(days=1),
+            end_datum=past_date,
+            ort="Test Ort"
+        )
+        past_event=Einreichung(fk=veranstaltung)
+        self.assertTrue(past_event.ist_in_vergangenheit())
+
+    # Soll anzeigen, was noch ist
+    def test_is_in_future(self):
+        """Gibt True zurück, wenn die Veranstaltung ab Heute ist."""
+        future_date= (timezone.now()).date()
+        veranstaltung=Veranstaltung.objects.create(
+            titel="Test event",
+            start_datum=future_date,
+            end_datum=future_date+datetime.timedelta(days=1),
+            ort="Test Ort"
+        )
+        future_event=Einreichung(fk=veranstaltung)
+        self.assertTrue(future_event.ist_in_zukunft())
 
 # Create your tests here.
